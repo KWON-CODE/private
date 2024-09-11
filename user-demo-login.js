9d1d9f48713aaf303ed771e150745726a15a1813
YEONGINKWON <duddls745@nate.com>
2024-09-11 11:56:28 +0900

    Update user-demo-login.js

--- user-demo-login.js
+++ user-demo-login.js
@@ -8,11 +8,38 @@ let db = new Map()
 var id = 1 //id라는 값을 하나의 객체를 특별하게 구별하기위함으로 사용
 
 //2. 로그인
+// POST 쓰는이유 GET을 사용하면 URL로 데이터를 보내야하는데 개인정보가 노출된다.
 
 app.post('/login', function(req, res){
+    console.log(req.body) //1. userId,pwd id가 db에 저장된 회원인지
+    //userId가 db에 저장된 회원인지... pwd도 맞는지 비교
+    let loginUser = {}
 
+    const {userId, password} = req.body
+    db.forEach(function(user, id) {
+        //a : 데이터   a:value = user위치
+        //b : 인덱스   b:key   =id 위치
+        //c : 객체    c:Map(통째로)
+        console.log(user.userId)
+        if (user.userId === userId) {
+          loginUser = user
 
+        } 
+    })
+   
+    if (loginUser) {
+        console.log("같은 것")
+         //pwd도 맞는지 비교
+         if (loginUser.password === password) {
+            console.log("패스워드 같다")
+         } else {
+            console.log("패스워드 틀렸다.")
+         }
+       
 
+    } else {
+        console.log('입력하신 아이디는 없는 아이디.')
+    }
 })
 
 //1. 회원가입
