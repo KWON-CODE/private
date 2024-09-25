49df1ff496d9cefd688df69d5c946ef94f7d5fd0
YEONGINKWON <duddls745@nate.com>
2024-09-25 22:36:42 +0900

    Create user.js

---
+++ user.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const router = express.Router()
+const conn = require('../mariadb')
+
+router.use(express.json()) 
+
+router.post('/login', function(req, res){
+    const {email, password} = req.body 
+
+    let sql =  `SELECT * FROM users WHERE email = ?`
+    
+    conn.query(sql, email,
+        function(err, results) {
+            let loginUser = results[0];
+              
+                if(loginUser && loginUser.password == password) {
+                    res.status(200).json({
+                      message : `${loginUser.name}님 로그인 되었습니다.`
+                   })
+                 } else {
+                res.status(404).json({
+                            message : `잘못된 이메일 또는 비밀번호`
+                  })
+              }
+            }
+     );
+
+})
+
+router.post('/join', function(req, res) {
+    if (req.body == {}) {
+         res.status(400).json ({
+            message : `입력 값을 다시 확인 부탁드립니다.`
+        })
+     } else {
+        const {email, name, password, contact} = req.body
+        
+        let sql = `INSERT INTO users (email, name, password, contact)
+                VALUES (?, ?, ?, ?)`
+        let values = [email, name, password, contact]
+        conn.query(sql, values,
+            function(err, results) {
+                 res.status(201).json(results)
+              }
+         );
+       
+    }  
+
+})
+
+router
+    .route('/users')
+    .get(function(req, res) {
+        let {email} = req.body
+       
+        let sql =  `SELECT * FROM users WHERE email = ?`
+        conn.query(sql, email,
+            function(err, results, fields) {
+                 res.status(200).json(results)
+               }
+         );
+     })
+    .delete(function(req, res) {
+        let {email} = req.body
+       
+        let sql = `DELETE FROM users WHERE email = ?`
+        conn.query(sql, email,
+            function(err, results) { 
+                res.status(200).json(results)   
+           }
+         );
+    })
+
+module.exports = router
