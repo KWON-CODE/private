cf2e004fc0d2f7a652354f5336ad3b215b0f6fd3
YEONGINKWON <duddls745@nate.com>
2024-10-04 08:26:50 +0900

    Update UserController.js

--- 도서API/controller/UserController.js
+++ 도서API/controller/UserController.js
@@ -1,5 +1,9 @@
 const conn = require('../mariadb'); // db모듈
 const {StatusCodes} = require('http-status-codes'); // status code 모듈
+const jwt = require('jsonwebtoken'); // jwt 모듈
+const dotenv = require('dotenv'); // dotenv 모듈
+dotenv.config();
+
 
 const join = (req,res) => {
     const {email, password} = req.body;
@@ -18,16 +22,84 @@ const join = (req,res) => {
 };
 
 const login = (req,res) => {
-    res.json('로그인');
+    const {email, password} = req.body;
+
+    let sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email,
+        (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+            }
+
+            const loginUser = results[0];
+            if(loginUser && loginUser.password == password) {
+                //토큰 발행
+                const token = jwt.sign({
+                    email : loginUser.email
+                }, process.env.PRIVATE_KEY, {
+                    expiresIn : '5m',
+                    issuer : "yeongin"
+                });
+                // 토큰 쿠키에 담기
+                res.cookie("token", token, {
+                    httpOnly : true
+                });
+                console.log(token);
+
+                return res.status(StatusCodes.OK).json(results);
+            
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).end(); //401 : Unauthorized 403 : Forbidden (접근 권한 없음)
+            }
+
+            
+        })
 };
 
 
 const passwordResetRequest = (req,res) => {
-    res.json('비밀번호 초기화 요청');
+    const {email} = req.body;
+
+    let sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email,
+        (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+            }
+
+            // 이메일로 유저가 있는지 찾아본다
+            const user = results[0];
+            if (user) {
+                return res.status(StatusCodes.OK).json({
+                    email : email
+                });
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+            }
+        }
+    )  
 };
 
 const passwordReset = (req,res) => {
-    res.json('비밀번호 초기화 요청');
+    const {email, password} = req.body;
+
+    let sql = 'UPDATE users SET password=? WHERE email=?';
+    let values = [password, email];
+    conn.query(sql, values,
+        (err, results) => {
+            if(err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+            }
+
+            if(results.affectedRows == 0)
+            return res.status(StatusCodes.BAD_REQUEST).end();
+            else
+            return res.status(StatusCodes.OK).json(results);
+        }
+    )
 };
 
 module.exports = {
